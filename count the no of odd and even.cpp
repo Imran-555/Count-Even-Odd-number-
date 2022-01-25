757af72c29fd7ae41ed3425e93dc6693b6fd4fc4
Imran-555 <74055781+Imran-555@users.noreply.github.com>
2022-01-25 13:01:05 +0530

    Add files via upload

---
+++ count the no of odd and even.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+ int main()
+ {
+ 	int a[10],i,j,k,e=0,o=0;
+ 	printf("enter the 10 number randomly ");
+ 	for (i=0;i<=9;i++)
+ 	{
+	 
+ 	scanf("%d",&a[i]);
+ 	if(a[i]%2==0)
+ 	{
+ 		e=e+1;
+	 }
+	 else
+	 {
+	 	o=o+1;
+	 }
+    } 
+	printf(" Even no is %d",e);
+	printf(" Odd no is %d",o);
+}
+	 
+ 
+
+	
+       
+    
+
+	 
+ 
+	 
+
+ 
+ 
+
+	
+		 	
+
+	 
+ 	
+
+	
+     
+  
